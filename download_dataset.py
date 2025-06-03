7471fe4946038067a3ca4823d058e7347d49af1d
MabelRincon <mabel.rincon@outlook.com>
2025-06-03 19:52:19 +0200

    refactor:create venv and .env to ofuscate api key

--- download_dataset.py
+++ download_dataset.py
@@ -1,7 +1,14 @@
 from roboflow import Roboflow
+import os
+from dotenv import load_dotenv
 
-rf = Roboflow(api_key="d7VSK4YxfjrKftXojiIN")
+load_dotenv()
+
+api_key = os.getenv('ROBOFLOW_API_KEY')
+if not api_key:
+    raise ValueError("ROBOFLOW_API_KEY not found in environment variables")
+
+rf = Roboflow(api_key=api_key)
 project = rf.workspace("iaespecializacion").project("hard_hat_detection-3onnc")
-version = project.version(2)
+version = project.version(3)
 dataset = version.download("yolov8")
-                
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # download YOLOv8 pretrained model
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8x.pt")
 
 # train the model with local dataset
-model.train(data="dataset/data.yaml", epochs=50, imgsz=640)
+model.train(data="hard_hat_detection-3/data.yaml", epochs=50, imgsz=640)
