31bb46819dff6f44e0b2a2a735b2bb04b0a04da1
MabelRincon <mabel.rincon@outlook.com>
2025-06-03 20:33:42 +0200

    fix: remove large files and update gitignore

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 
-# download YOLOv8 pretrained model
 model = YOLO("yolov8x.pt")
 
-# train the model with local dataset
 model.train(data="hard_hat_detection-3/data.yaml", epochs=50, imgsz=640)
